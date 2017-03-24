844324f1577bc473030c374de5659f7655138331
John Sekar <johnarul.sekar@gmail.com>
2017-03-23 21:37:49 -0400

    clean up

--- structures.py
+++ structures.py
@@ -1,6 +1,7 @@
 from wc_utils.schema import core
 import inspect
 
+###### Structures ######
 class Complex(core.Model):
 	id = core.StringAttribute(primary=True,unique=True)
 	def getMolecule(self,label,**kwargs):
@@ -31,29 +32,44 @@ class Bond(core.Model):
 	class Meta(core.Model.Meta):
 		attribute_order = ('id','complex','linkedsites', )
 
+###### Variables ######
+
+###### Operations ######
 class Operation(core.Model):
 	id = core.StringAttribute(primary=True,unique=True)
 
 class AddBond(Operation):
+	label = core.StringAttribute(unique=False,default='AddBond')
 	linkedsites = core.OneToManyAttribute(Site,related_name='addbond')
 	
 class DeleteBond(Operation):
+	label = core.StringAttribute(unique=False,default='DeleteBond')
 	linkedsites = core.OneToManyAttribute(Site,related_name='deletebond')
 	
 class AddMol(Operation):
+	label = core.StringAttribute(unique=False,default='AddMol')
 	mol = core.OneToOneAttribute(Molecule,related_name='addmol')
 
 class DeleteMol(Operation):
+	label = core.StringAttribute(unique=False,default='DeleteMol')
 	mol = core.OneToOneAttribute(Molecule,related_name='deletemol')
 	
 class Rule(core.Model):
 	id = core.StringAttribute(primary=True,unique=True)
-	label = core.StringAttribute(unique=False)
+	label = core.StringAttribute(unique=True)
 	reactants = core.OneToManyAttribute(Complex,related_name='rule')
 	operations = core.OneToManyAttribute(Operation,related_name='rule')
-
+	reversible = core.BooleanAttribute(default=False)
+	
+###### Structure Improvements ######
 class Protein(Molecule): pass
 class ProteinSite(Site): pass
+
+###### Variable Improvements ######
+
+###### Operation Improvements ######
+
+
 
 def main():
 	class Lig(Protein):
@@ -62,19 +78,40 @@ def main():
 		label = core.StringAttribute(unique=False,default='rec')
 	class Rec(Protein):
 		label = core.StringAttribute(unique=False,default='Rec')
+		ph1 = core.BooleanAttribute(default=False)
+		ph2 = core.BooleanAttribute(default=False)
 	class lig(ProteinSite):
 		label = core.StringAttribute(unique=False,default='lig')
 
-	x = Complex(id='1',molecules=[ Lig(sites=[rec(id='1'),rec(id='2')]) ])
-	y = Complex(id='1',molecules=[ Rec(sites=[lig()]) ])
+
+	### Rule R1: Ligand-binding ###
+	m1 = Lig(sites = [ rec(id='1'),rec(id='2') ] )
+	m2 = Rec(sites = [ lig() ] )
+	c1 = Complex(molecules=[m1])
+	c2 = Complex(molecules=[m2])
+	op = AddBond(linkedsites = [
+					m1.getSite('rec',id='1'),
+					m2.getSite('lig')
+					])
 	
-	op = AddBond(id='1',linkedsites=[
-		x.molecules.get(label='Lig').sites.get(label='rec',id='1'),
-		y.molecules.get(label='Rec').sites.get(label='lig'),
-		] ) 
+	rule1 = Rule(label='R1',reactants=[c1,c2],operations=[op],reversible=True)
 	
-	rule1 = Rule(label='R1',reactants=[x,y],operations=[op])
 	
+	### Rule R2: Crosslinking ###
+	m1 = Lig(sites = [ rec(id='1'),rec(id='2') ] )
+	m2 = Rec(sites = [ lig() ] )
+	m3 = Rec(sites = [ lig() ] )
+	b1 = Bond(linkedsites=[
+				m1.getSite('rec',id='1'),
+				m2.getSite('lig') 
+			])
+	op = AddBond(linkedsites=[
+				m1.getSite('rec',id='2'),
+				m3.getSite('lig') 
+			])
+	reac1 = Complex(molecules = [m1,m2],bonds=[b1])
+	reac2 = Complex(molecules = [m3])
+	rule2 = Rule(label='R2',reactants = [reac1,reac2],operations = [op],reversible=True)
 	
 	
 if __name__ == '__main__':
