f2e220f3c03ebcca81fd9921a4563bc170a1a06f
John Sekar <johnarul.sekar@gmail.com>
2017-04-06 19:27:42 -0400

    Stubs for tests

--- tests/test_wc_rules.py
+++ tests/test_wc_rules.py
@@ -2,5 +2,14 @@ from wc_rules import bioschema
 import unittest
 
 class TestBioschema(unittest.TestCase):
-    def test_example(self):
-        self.assertTrue(True)
+	def test_site(self):pass
+	def test_bond(self):pass
+	def test_exclusion(self):pass
+	def test_state(self):pass
+	def test_molecule(self):pass
+	def test_complex(self):pass
+	def test_bond_op(self):pass
+	def test_state_op(self):pass
+	def test_rule(self):pass
+	def test_example(self):
+		self.assertTrue(True)
