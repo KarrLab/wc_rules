0254220337313ef19bef48efdb13be702433948f
John Sekar <johnarul.sekar@gmail.com>
2017-05-31 06:40:42 -0400

    Updated graph matching algos

--- wc_rules/graph_utils.py
+++ wc_rules/graph_utils.py
@@ -4,7 +4,7 @@ import networkx as nx
 class GraphMeta(core.Model.Meta):
 	""" Inner class holding values used in graph methods
 	Attributes:
-	* outward_edges (:obj:`str`): attributes to be examined recursively for get_edges(). 
+	* outward_edges (:obj:`str`): attributes to be examined recursively for get_graph(). 
 	Only RelatedManager attributes allowed.
 	* semantic (:obj:`str`): attributes/properties should be examined for node_match(). 
 	Only attributes/properties that return values comparable by '==' allowed.	
@@ -15,31 +15,32 @@ class GraphMeta(core.Model.Meta):
 def node_match(node1,node2):
 	#### this is to be compatible with networkx's subgraph isomorphism algorithm
 	# node2 is from the smaller graph
-	# node1 is from the bigger graph
+	# node1 is from the bigger graph, whose subgraphs are being questioned
 	return node_compare(node2['obj'],node1['obj'])
+	
+def edge_match(dict1,dict2):
+	return dict1 == dict2
+
+class GraphMatcher(nx.algorithms.isomorphism.DiGraphMatcher):
+	def __init__(self,g1,g2):
+		super(GraphMatcher,self).__init__(g1,g2,node_match=node_match,edge_match=edge_match)
 
 def node_compare(current,other):
 	#### Logic: can current node's semantic properties be entirely contained within other node?
-	# current --- None returns False
-	# current --- other returns False if 
-	# 	other is not an instance of type(current) or any subclass thereof
-	# current --- other returns False if	
-	# 	attributes in 'semantic' list (see GraphMeta class) do not match in value, i.e.
-	#		None --- anything is a match
-	#		current.attrib --- None is not a match
-	#		current.attrib == current.attrib is a match
+	# other must be an instance of current.__class__ or any of its subclasses
+	# current's attributes, if specified, must entirely match other's
 	
-	match = True
-	if other is None: match = False
-	if isinstance(other,(current.__class__,)) is not True:
-		match = False
-	for attrname in current.__class__.GraphMeta.semantic:
-		current_attr = getattr(current,attrname)
-		other_attr = getattr(other,attrname)
-		if current_attr is not None:
-			if other_attr is None: match = False
-		if current_attr != other_attr: match = False
-	#print('current=',current,',other=',other,',match=',match)
+	def type_match(current,other):
+		return isinstance(other,(current.__class__,))
+	
+	def attr_match(current,other,attrname):
+		is_unspecified = getattr(current,attrname,None) is None
+		is_matched = getattr(current,attrname,None)==getattr(other,attrname,None)
+		return is_unspecified or is_matched
+
+	attrlist = current.__class__.GraphMeta.semantic
+	match = type_match(current,other) and all( attr_match(current,other,x) for x in attrlist )
+	
 	return match
 	
 def get_graph(current_obj,recurse=True,memo=None):
@@ -55,7 +56,8 @@ def get_graph(current_obj,recurse=True,memo=None):
 	# Adding node if not already in memo, else updating it
 	update_graph(memo,current_obj)
 	
-	# getting list of next nodes to check
+	# getting list of next nodes to check 
+	# and the relationship to those nodes
 	next_nodes = []
 	node_relation = {}
 	for attrname in current_obj.__class__.GraphMeta.outward_edges:
@@ -68,6 +70,7 @@ def get_graph(current_obj,recurse=True,memo=None):
 			else:
 				next_nodes.append(attr)
 				node_relation[attr] = attrname 
+	
 	# for each node in next_nodes,
 	# if an edge already exists, ignore
 	# if adding a new edge, recurse onto that node using current memo
