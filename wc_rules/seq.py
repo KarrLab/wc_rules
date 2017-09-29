067975b8bc0c9aae6f845cd4f97419671bc5200e
John Sekar <johnarul.sekar@gmail.com>
2017-09-29 12:23:09 -0400

    Methods to add SequenceSites and compute overlaps

--- wc_rules/seq.py
+++ wc_rules/seq.py
@@ -1,26 +1,55 @@
 from wc_rules.chem import Molecule, Site
 from obj_model.extra_attributes import *
 from Bio.Seq import Seq
+from Bio import SeqFeature
+from numpy import full
+import itertools
 
 class SequencePropertyAttribute(NumpyArrayAttribute): pass
 
 class SequenceMolecule(Molecule):
 	seq = BioSeqAttribute()
 
-	def __init__(self,**kwargs):
-		super(SequenceMolecule,self).__init__(**kwargs) 
+	def add_sequence(self,seq):
+		alphabet = self.Meta.attributes['seq'].alphabet
+		self.seq = Seq(seq,alphabet)
+		return self
+	def initialize_SequencePropertyAttribute(self,attrname):
+		dim = len(self.seq)
+		attr = self.Meta.attributes[attrname]
+		default_fill = attr.default_fill
+		dtype = attr.dtype
+		init_vals = full(dim,default_fill,dtype)
+		setattr(self,attrname,init_vals)
+		return self
+		
+	def initialize_properties(self):
 		if self.seq is not None and len(self.seq)>0:
 			for attrname,attr in self.Meta.attributes.items():
 				if isinstance(attr,SequencePropertyAttribute):
-					setattr(self,attrname,attr.get_default(self,length=len(self.seq)))
-					
-			
+					self.initialize_SequencePropertyAttribute(attrname)
+		return self
+		
+	def sort_sites(self):
+		self.sites = sorted(self.sites, key = lambda x: (x.location.start,x.location.end))
+		return self
+		
+	def compute_overlaps(self):
+		for site1,site2 in itertools.combinations(self.sites,2):
+			if (site1.location.start in site2.location) or (site1.location.end in site2.location):
+				site1.add_overlaps(site2)
+		return self
+		
+	
+	
 class DSDNA(SequenceMolecule):
 	seq = BioDnaSeqAttribute()
 	binding_footprint = SequencePropertyAttribute(dtype=bool,default_fill=False)
-	strand_break = SequencePropertyAttribute(dtype=bool,default_fill=False)
 
 			
+class SequenceSite(Site):
+	location = FeatureLocationAttribute()
+	
 			
 def main():
 	pass
