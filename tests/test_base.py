5bef970fdb824b1a69415820e1bbb9f3cfbebb62
John Sekar <johnarul.sekar@gmail.com>
2017-10-30 20:59:03 -0400

    Modified test_base to accommodate future changes.

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,7 +10,7 @@ class Person(base.BaseClass):
 	class GraphMeta(g.GraphMeta):
 		outward_edges=('children','pets')
 		semantic = ('name',)
-		
+
 class Pet(base.BaseClass):
 		name = core.StringAttribute()
 
@@ -20,15 +20,15 @@ class TestBase(unittest.TestCase):
 		self.Sherlock = Person(name='Sherlock')
 		self.John = Person(name='John')
 		self.Mary = Person(name='Mary')
-		
+
 		self.Kid001 = Person(name='Kid001')
 		self.Kid002 = Person(name='Kid002')
 		self.Dog001 = Pet(name='Dog001')
-	
+
 	@staticmethod
 	def list_names(xlist):
 		return list(map((lambda x:x.name),xlist))
-		
+
 	def test_ids(self):
 		all_things = [self.Sherlock, self.John, self.Mary, self.Kid001, self.Kid002, self.Dog001]
 		id_arr = [x.id for x in all_things]
@@ -42,11 +42,12 @@ class TestBase(unittest.TestCase):
 		]
 		# works only when base.idgen.seed(0) in setUp()
 		self.assertEqual(id_arr,check_str)
-	
+
 	def test_attribute_properties(self):
 		attrprops = self.John.attribute_properties
 		keylist = 'id name parents pets children'.split()
-		self.assertEqual(list(attrprops.keys()),keylist)
+		for x in keylist:
+			self.assertTrue(list(attrprops.keys()))
 		check_dict = {
 			'id':{'related': False, 'append': False},
 			'name': {'related': False, 'append': False},
@@ -57,64 +58,64 @@ class TestBase(unittest.TestCase):
 		for x in check_dict:
 			for y in ['related','append']:
 				self.assertEqual(attrprops[x][y],check_dict[x][y])
-				
+
 		self.assertEqual(attrprops['id']['related_class'],None)
 		self.assertEqual(attrprops['name']['related_class'],None)
 		self.assertEqual(attrprops['parents']['related_class'],type(self.John))
 		self.assertEqual(attrprops['pets']['related_class'],type(self.Dog001))
 		self.assertEqual(attrprops['children']['related_class'],type(self.Kid001))
-		
+
 	def test_addable_classes(self):
 		addable_classes = self.John.addable_classes
 		cls1 = type(self.John)
 		cls2 = type(self.Dog001)
-		
+
 		self.assertEqual(addable_classes[cls1]['attrnames'],['parents','children'])
 		self.assertEqual(addable_classes[cls2]['attrnames'],['pets'])
-		
+
 		self.assertFalse(addable_classes[cls1]['easy_add'])
 		self.assertTrue(addable_classes[cls2]['easy_add'])
-		
+
 	def test_basic_methods(self):
 		Kids = [self.Kid001,self.Kid002]
 		Parents = [self.John, self.Mary]
-		
+
 		with self.assertRaises(utils.FindError): self.John.find_attr_by_name('car')
-		
+
 		for x in Kids:
 			for y in Parents:
 				x.add_by_attrname(y,'parents')
-			
+
 		self.assertEqual(self.list_names(self.Kid001.parents),'John Mary'.split())
-		self.assertEqual(self.list_names(self.John.children),'Kid001 Kid002'.split())		
-		
+		self.assertEqual(self.list_names(self.John.children),'Kid001 Kid002'.split())
+
 		# this should raise an error in obj_model
-		#with self.assertRaises(utils.AddError): 
+		#with self.assertRaises(utils.AddError):
 		#	self.Kid001.add_by_attrname(self.Sherlock,'parents')
-		
+
 		for y in Kids:
 			for x in Parents:
 				y.remove_by_attrname(x,'parents')
-		
+
 		self.assertEqual(self.list_names(self.Kid001.parents),[])
 		self.assertEqual(self.list_names(self.John.children),[])
-		
+
 		with self.assertRaises(utils.RemoveError):
 			self.Kid001.remove_by_attrname(self.Sherlock,'parents')
-		
+
 		self.Dog001.set_by_attrname(self.Sherlock,'owner')
 		self.assertEqual(self.Dog001.owner.name,'Sherlock')
 		self.assertEqual(self.list_names(self.Sherlock.pets),['Dog001'])
-		
+
 		with self.assertRaises(utils.SetError):
 			self.John.set_by_attrname(self.Dog001,'children')
-			
+
 		with self.assertRaises(utils.SetError):
 			self.Sherlock.unset_by_attrname('pets')
 		self.Dog001.unset_by_attrname('owner')
 		self.assertEqual(self.Sherlock.pets,[])
 		self.assertEqual(self.Dog001.owner,None)
-		
+
 		self.Kid001.iadd([self.John,self.Mary],'parents')
 		x = self.list_names(self.Kid001.filter_by_attrname('parents',label='Person'))
 		self.assertEqual(x, 'John Mary'.split())
@@ -126,15 +127,15 @@ class TestBase(unittest.TestCase):
 		self.assertEqual(x, None)
 		with self.assertRaises(ValueError):
 			self.Kid001.get_by_attrname('parents')
-		
+
 		self.Kid001.iremove([self.John,self.Mary])
-		
-		
-		
+
+
+
 	def test_intelligent_methods(self):
 		attrlist = self.John.get_compatible_attribute_names(self.Sherlock)
 		self.assertEqual(attrlist,'parents children'.split())
-		
+
 		with self.assertRaises(utils.AddError):
 			self.John.iadd(object())
 		with self.assertRaises(utils.AddError):
@@ -143,27 +144,27 @@ class TestBase(unittest.TestCase):
 		self.assertEqual(self.list_names(self.Kid001.parents),'John Mary'.split())
 		self.Dog001.iadd(self.Sherlock)
 		self.assertEqual(self.Dog001.owner.name,'Sherlock')
-		
+
 		with self.assertRaises(utils.RemoveError):
 			self.Kid001.iremove(self.Sherlock)
-			
+
 		self.Kid001.iremove([self.John,self.Mary])
 		self.assertEqual(self.Kid001.parents,[])
 		self.Dog001.iremove(self.Sherlock)
 		self.assertEqual(self.Dog001.owner,None)
-			
+
 		self.John.iadd(self.Sherlock,'parents')
 		self.John.iadd(self.Sherlock,'children')
 		self.assertTrue(self.Sherlock in self.John)
 		self.assertEqual(self.John.attributes_that_contain(self.Sherlock),'parents children'.split())
-		
+
 		with self.assertRaises(utils.RemoveError):
 			self.John.iremove(self.Sherlock)
-			
+
 		self.John.iremove(self.Sherlock,'parents')
 		self.John.iremove(self.Sherlock,'children')
 		self.assertTrue(self.Sherlock not in self.John)
-		
+
 		self.Kid001.iadd([self.John,self.Mary],'parents')
 		x = self.list_names(self.Kid001.ifilter(label='Person'))
 		self.assertEqual(x, 'John Mary'.split())
@@ -176,4 +177,3 @@ class TestBase(unittest.TestCase):
 		with self.assertRaises(utils.FindError):
 			self.Kid001.iget(label='Person')
 		self.Kid001.iremove([self.John,self.Mary])
-		
