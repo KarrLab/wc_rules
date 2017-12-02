cf214ae6651f40ecb642a69c1a08d16a9998259a
Jonathan Karr <jonrkarr@gmail.com>
2017-12-02 12:41:04 -0500

    Cleaning up packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,18 @@ pip.main(['install', 'git+https://github.com/KarrLab/wc_utils.git#egg=wc_utils']
 
 from setuptools import setup, find_packages
 from wc_utils.util.install import parse_requirements, install_dependencies
-import wc_rules
+import os
+
+# get long description
+if os.path.isfile('README.rst'):
+    with open('README.rst', 'r') as file:
+        long_description = file.read()
+else:
+    long_description = ''
+
+# get version
+with open('wc_rules/VERSION', 'r') as file:
+    version = file.read().strip()
 
 # parse dependencies and links from requirements.txt files
 with open('requirements.txt', 'r') as file:
@@ -18,16 +29,21 @@ install_dependencies(dependency_links)
 # install package
 setup(
     name="wc_rules",
-    version=wc_rules.__version__,
+    version=version,
     description="Language for describing whole-cell models",
+    long_description=long_description,
     url="https://github.com/KarrLab/wc_rules",
-    download_url='https://github.com/KarrLab/wc_rules/tarball/{}'.format(wc_rules.__version__),
+    download_url='https://github.com/KarrLab/wc_rules',
     author="John Sekar",
     author_email="johnarul.sekar@gmail.com",
     license="MIT",
     keywords='whole-cell systems biology',
     packages=find_packages(exclude=['tests', 'tests.*']),
-    package_data={},
+    package_data={
+        'wc_rules': [
+            'VERSION',
+        ],
+    },
     install_requires=install_requires,
     tests_require=tests_require,
     dependency_links=dependency_links,
