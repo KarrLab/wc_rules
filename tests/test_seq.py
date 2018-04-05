3d6e6e61acddf544188394b718bb3df0c0a0ba51
John Sekar <johnarul.sekar@gmail.com>
2018-04-05 17:23:47 -0400

    Testing initialization of SequenceMolecule and its subclasses

---
+++ tests/test_seq.py
@@ -0,0 +1,40 @@
+"""
+:Author: John Sekar <johnarul.sekar@gmail.com>
+:Date: 2017-04-05
+:Copyright: 2017, Karr Lab
+:License: MIT
+"""
+
+from wc_rules import bio,utils
+import unittest
+
+
+class TestSeq(unittest.TestCase):
+
+    def test_sequence_init(self):
+        X = bio.DNASequenceMolecule().init_sequence('ATCGR')
+        self.assertEqual(X.sequence,'ATCGR')
+        with self.assertRaises(utils.SeqError):
+            X = bio.DNASequenceMolecule().init_sequence('ATCGR',ambiguous=False)
+        X = bio.DNASequenceMolecule().init_sequence('ATCG',ambiguous=False)
+        self.assertEqual(X.sequence,'ATCG')
+        with self.assertRaises(utils.SeqError):
+            X = bio.DNASequenceMolecule().init_sequence('ZZZZ')
+
+        X = bio.RNASequenceMolecule().init_sequence('AUCGR')
+        self.assertEqual(X.sequence,'AUCGR')
+        with self.assertRaises(utils.SeqError):
+            X = bio.RNASequenceMolecule().init_sequence('AUCGR',ambiguous=False)
+        X = bio.RNASequenceMolecule().init_sequence('AUCG',ambiguous=False)
+        self.assertEqual(X.sequence,'AUCG')
+        with self.assertRaises(utils.SeqError):
+            X = bio.RNASequenceMolecule().init_sequence('ZZZZ')
+
+        X = bio.ProteinSequenceMolecule().init_sequence('ACDEFBXZ')
+        self.assertEqual(X.sequence,'ACDEFBXZ')
+        with self.assertRaises(utils.SeqError):
+            X = bio.ProteinSequenceMolecule().init_sequence('ACDEFBXZ',ambiguous=False)
+        X = bio.ProteinSequenceMolecule().init_sequence('ACDEF',ambiguous=False)
+        self.assertEqual(X.sequence,'ACDEF')
+        with self.assertRaises(utils.SeqError):
+            X = bio.ProteinSequenceMolecule().init_sequence('1234')
