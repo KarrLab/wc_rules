45ec9acfa73672e78f90ed9167fc53e252f26e05
John Sekar <johnarul.sekar@gmail.com>
2018-04-05 17:22:21 -0400

    Commenting out old methods in seq.py

--- wc_rules/seq.py
+++ wc_rules/seq.py
@@ -12,48 +12,50 @@ import itertools
 import numpy
 
 
-class SequencePropertyAttribute(extra_attributes.NumpyArrayAttribute):
-    pass
 
 
-class SequenceMolecule(chem.Molecule):
-    seq = extra_attributes.BioSeqAttribute()
 
-    def add_sequence(self, seq):
-        alphabet = self.Meta.attributes['seq'].alphabet
-        self.seq = Bio.Seq.Seq(seq, alphabet)
-        return self
-
-    def initialize_SequencePropertyAttribute(self, attrname):
-        dim = len(self.seq)
-        attr = self.Meta.attributes[attrname]
-        default_fill = attr.default_fill
-        dtype = attr.dtype
-        init_vals = numpy.full(dim, default_fill, dtype)
-        setattr(self, attrname, init_vals)
-        return self
-
-    def initialize_properties(self):
-        if self.seq is not None and len(self.seq) > 0:
-            for attrname, attr in self.Meta.attributes.items():
-                if isinstance(attr, SequencePropertyAttribute):
-                    self.initialize_SequencePropertyAttribute(attrname)
-        return self
-
-    def sort_sites(self):
-        self.sites = sorted(self.sites, key=lambda x: (x.location.start, x.location.end))
-        return self
-
-    def compute_overlaps(self):
-        for site1, site2 in itertools.combinations(self.sites, 2):
-            if (site1.location.start in site2.location) or (site1.location.end in site2.location):
-                site1.add_overlaps(site2)
         return self
 
 
-class DSDNA(SequenceMolecule):
-    seq = extra_attributes.BioDnaSeqAttribute()
-    binding_footprint = SequencePropertyAttribute(dtype=bool, default_fill=False)
+    #class SequencePropertyAttribute(extra_attributes.NumpyArrayAttribute):
+    #    pass
+
+
+    # def add_sequence(self, seq, seqtype, unambiguous=False):
+    #     alphabet = self.Meta.attributes['seq'].alphabet
+    #     self.seq = Bio.Seq.Seq(seq, self.alphabet)
+    #     return self
+    #
+    # def initialize_SequencePropertyAttribute(self, attrname):
+    #     dim = len(self.seq)
+    #     attr = self.Meta.attributes[attrname]
+    #     default_fill = attr.default_fill
+    #     dtype = attr.dtype
+    #     init_vals = numpy.full(dim, default_fill, dtype)
+    #     setattr(self, attrname, init_vals)
+    #     return self
+    #
+    # def initialize_properties(self):
+    #     if self.seq is not None and len(self.seq) > 0:
+    #         for attrname, attr in self.Meta.attributes.items():
+    #             if isinstance(attr, SequencePropertyAttribute):
+    #                 self.initialize_SequencePropertyAttribute(attrname)
+    #     return self
+
+    # def sort_sites(self):
+    #     self.sites = sorted(self.sites, key=lambda x: (x.location.start, x.location.end))
+    #     return self
+    #
+    # def compute_overlaps(self):
+    #     for site1, site2 in itertools.combinations(self.sites, 2):
+    #         if (site1.location.start in site2.location) or (site1.location.end in site2.location):
+    #             site1.add_overlaps(site2)
+    #     return self
+    #
+    #        binding_footprint = SequencePropertyAttribute(dtype=bool, default_fill=False)
+    #
+
 
 
 class SequenceSite(chem.Site):
