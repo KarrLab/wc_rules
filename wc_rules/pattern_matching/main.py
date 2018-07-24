adc72384b11d919eea9fa3f45b7256ce587dd2c3
John Sekar <johnarul.sekar@gmail.com>
2018-07-24 03:58:26 -0400

    Rete network TypeCheck nodes and AttrCheck nodes

--- wc_rules/pattern_matching/main.py
+++ wc_rules/pattern_matching/main.py
@@ -1,17 +1,17 @@
-from core import WorkingMemoryGraph
+from core import WorkingMemoryGraph, AlphaTree,Pattern
 from wc_rules.chem import Molecule,Site
 from obj_model.core import BooleanAttribute
 
-from random import random
+import random
 import pprint
 
-def boolgen(): return random() < 0.5
+def boolgen(): return random.random() < 0.5
 
 class A(Molecule):pass
 
 class X(Site):
-    ph1 = BooleanAttribute(default=False)
-    ph2 = BooleanAttribute(default=False)
+    ph1 = BooleanAttribute(default=None)
+    ph2 = BooleanAttribute(default=None)
 
 
 def object_generator(n=1):
@@ -20,19 +20,25 @@ def object_generator(n=1):
 
 
 list_of_molecules = object_generator(5)
-
-'''
-for i in list_of_molecules:
-    print(i.label, i.id)
-    for x in i.sites:
-        print(x.label,x.id,x.ph1,x.ph2)
-    print('')
-'''
-
 wmg = WorkingMemoryGraph()
 for molec in list_of_molecules:
     wmg.add_node(molec)
     for site in molec.sites:
         wmg.add_node(site)
 
-pprint.pprint(wmg.nodes)
+a1 = A(id='a').add_sites(X(id='x',ph1=True))
+patt1 = Pattern('p1').add_nodes([a1])
+
+a2 = A(id='a').add_sites(X(id='x',ph2=True))
+patt2 = Pattern('p2').add_nodes([a2])
+
+a3 = A(id='a').add_sites(X(id='x',ph1=True,ph2=True))
+patt3 = Pattern('p3').add_nodes([a3])
+
+
+typecheck_tree = AlphaTree().add_patterns([patt1,patt2,patt3])
+
+for idx,instance in wmg.nodes.items():
+    typecheck_tree.process_token(instance,verbose=True)
+    print()
+#pprint.pprint(a1.sites[0].attribute_properties)
