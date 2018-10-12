f73174b8f75d8e8a82ff616ef6f697e9344c6c8d
John Sekar <johnarul.sekar@gmail.com>
2018-10-12 16:03:28 -0400

    Revert "RestrictedDict class to create token content"
    This reverts commit 6f215853f0a3431388f1fd05ee3fc46bf48604cb.

--- wc_rules/rete_token.py
+++ wc_rules/rete_token.py
@@ -1,3 +1,20 @@
-from .indexer import RestrictedDict
+from .utils import generate_id
+import copy
 
-class Content(RestrictedDict):pass
+class ReteToken(object):
+    def __init__(self,id=None,level=None,sender=None,vardict=None):
+        if id is None:
+            id = generate_id()
+        if level is None:
+            level = 0
+        self.id = id
+        self.level = level
+        self.sender = sender
+        self.vardict = vardict
+
+    def duplicate(self,sender=None):
+        tok = copy.copy(self)
+        tok.level += 1
+        if sender is not None:
+            tok.sender = sender
+        return tok
