42545ce0d42630a6040fe3c3934811cea825b1da
John Sekar <johnarul.sekar@gmail.com>
2018-11-03 23:42:04 -0400

    Moved stuff around

--- wc_rules/euler_tour.py
+++ wc_rules/euler_tour.py
@@ -9,6 +9,7 @@ class EulerTour(object):
         self._edges = edges if edges is not None else set()
         self._spares = spares if spares is not None else set()
 
+    # Magic methods
     def __contains__(self,node):
         return node in self._tour
 
@@ -18,6 +19,21 @@ class EulerTour(object):
     def __str__(self):
         return ' '.join([x.id for x in self._tour]+['spares =',str(len(self._spares))])
 
+    # Static methods for handling edges
+    def flip(edge):
+        return tuple(reversed(edge))
+    def canonize(edge):
+        node1,attr1,attr2,node2 = edge
+        as_is = (attr1 <= attr2) or (attr1==attr2 and node1.id<node2.id)
+        if not as_is:
+            return self.flip(edge)
+        return edge
+
+    # Search methods
+    def first_occurrence(self,node):
+        if node in self:
+            return self._tour.index(node)
+        return None
     def last_occurrence(self,node):
         if node in self:
             for i in range(len(self),-1,-1):
@@ -25,11 +41,7 @@ class EulerTour(object):
                     return i
         return None
 
-    def first_occurrence(self,node):
-        if node in self:
-            return self._tour.index(node)
-        return None
-
+    # Basic modifications
     def reroot(self,node):
         # tour is a blist
         if self._tour[0]==node:
@@ -39,28 +51,21 @@ class EulerTour(object):
         self._tour = tour[i:] + tour[1:i] + [tour[i]]
         return self
 
-    def extend_right(self,nodes):
-        self.insert_sequence(len(self),nodes)
-        return self
-    def extend_left(self,nodes):
-        self.insert_sequence(0,nodes)
-        return self
     def insert_sequence(self,idx,nodes):
         assert 0 <= idx <= len(self)
         self._tour = self._tour[:idx] + blist(nodes) + self._tour[idx:]
         return self
+
     def delete_sequence(self,idx,length):
         assert 0 <= idx < idx+length <= len(self)
         self._tour = self._tour[:idx] + self._tour[idx+length:]
         return self
 
+    def extend_left(self,nodes):
+        self.insert_sequence(0,nodes)
+        return self
 
-    def flip(edge):
-        return tuple(reversed(edge))
-
-    def canonize(edge):
-        node1,attr1,attr2,node2 = edge
-        as_is = (attr1 <= attr2) or (attr1==attr2 and node1.id<node2.id)
-        if not as_is:
-            return self.flip(edge)
-        return edge
+    def extend_right(self,nodes):
+        self.insert_sequence(len(self),nodes)
+        return self
+        
