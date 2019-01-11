2087e835c6d236005677c55ce182ba8e08a1b045
John Sekar <johnarul.sekar@gmail.com>
2019-01-10 22:03:52 -0500

    get dependencies

--- wc_rules/expr2.py
+++ wc_rules/expr2.py
@@ -1,4 +1,5 @@
 from lark import Lark, tree, Transformer,Visitor, v_args
+from collections import defaultdict
 from pprint import pformat, pprint
 # DO NOT USE CAPS
 grammar = """
@@ -7,19 +8,18 @@ grammar = """
 %import common.NUMBER
 %ignore WS
 %import common.ESCAPED_STRING
-%ignore WS
 
 COMMENT: /#.*/
 %ignore COMMENT
 
-    ?number.1: NUMBER -> number | "True"-> true | "False" -> false | ESCAPED_STRING -> string
-    ?atom: number | function_call | "(" sum ")" 
+    ?literal.1: NUMBER -> number | "True"-> true | "False" -> false | ESCAPED_STRING -> string
+    ?atom: literal | function_call | "(" expression ")" 
 
-    function_name: "any" -> anyfunc | "all" -> allfunc | "not" -> notfunc | CNAME
+    function_name: CNAME
     variable: CNAME
     attribute: CNAME
 
-    arg: expression
+    arg: expression | boolean_expression
     kw: CNAME
     kwarg: kw "=" arg
     args: arg ("," arg )*
@@ -27,8 +27,6 @@ COMMENT: /#.*/
     
     function_call: variable "." function_name "(" [kwargs] ")"
         | function_name "(" [args] ")"
-        | function_name "(" [boolargs] ")" 
-        | match_count
         | variable "." attribute 
         | variable
         
@@ -42,13 +40,11 @@ COMMENT: /#.*/
 
     ?expression: sum
 
-    boolean_expression: expression bool_op expression -> comparebool
-        | match_expression -> matchbool
-        | expression -> defaultbool
-
+    ?boolean_expression: expression bool_op expression -> comparebool
+        | match_expression 
+        | expression 
 
     ?bool_op: ">=" -> geq | "<=" -> leq | ">" -> ge | "<" -> le | "==" -> eq | "!=" -> ne
-    boolargs: boolean_expression ("," boolean_expression)*
     
     pattern: CNAME
     pattern_variable:CNAME
@@ -57,10 +53,58 @@ COMMENT: /#.*/
     match_expression: "{" varpairs "}" "in" pattern
     match_count: "count" "(" match_expression ")"
 
-    assignment: variable "=" (expression|boolean_expression)
+    assignment: declared_variable "=" (expression|boolean_expression)
+    declared_variable: CNAME
 
-    expressions: (assignment|boolean_expression)*
-    start:expressions
+    expressions:  (assignment|boolean_expression)* 
+    ?start: expressions
 """
 
 parser = Lark(grammar, start='start')
+
+def node_to_str(node):
+    return node.children[0].__str__()
+
+def get_dependencies(tree):
+    deplist = []
+    for expr in tree.children:
+        deps = defaultdict(set)
+        # first get all function call nodes
+        if expr.data=='assignment':
+            var = node_to_str(expr.children[0])
+            deps['assignments'].add(var)
+        funcnodes = expr.find_pred(lambda x: x.data=='function_call')
+        for node in funcnodes:
+            ch = node.children
+            function_call_type = tuple([x.data for x in ch])
+            if function_call_type == ('variable',):
+                var = node_to_str(ch[0])
+                deps['variables'].add(var)
+            elif function_call_type == ('variable','attribute'):
+                var,attr = [node_to_str(x) for x in ch]
+                deps['variables'].add(var)
+                deps['attributes'].add( (var,attr) )
+            elif function_call_type == ('variable','function_name'):
+                var,func = [node_to_str(x) for x in ch]
+                deps['variables'].add(var)
+                deps['varfunctions'].add( (var,func) )
+            elif function_call_type == ('variable','function_name','kwargs'):
+                var,func,_ = [node_to_str(x) for x in ch]
+                kwargs = ch[2].children
+                kws = tuple(sorted([node_to_str(x.children[0]) for x in kwargs]))
+                deps['variables'].add(var)
+                deps['varfunctions'].add( (var,func,kws) )
+            elif function_call_type  in [('function_name','args'),('function_name',)]:
+                function_name = node_to_str(ch[0])
+                deps['builtins'].add(function_name)
+        matchnodes = expr.find_pred(lambda x: x.data == 'match_expression')
+        for node in matchnodes:
+            pattern = node_to_str(node.children[1])
+            deps['patterns'].add(pattern)
+            varpairs = node.children[0].children
+            pvars = []
+            for varpair in varpairs:
+                pvars.append(node_to_str(varpair.children[0]))
+            deps['patternvars'].add( tuple([pattern,tuple(pvars)]))
+        deplist.append(deps)
+    return deplist
