48ef02b4836da691f3bba66229e6998a6f7a7169
John Sekar <johnarul.sekar@gmail.com>
2020-05-26 05:41:13 -0400

    canonize: loop over partition, checking backward

--- wc_rules/canonical.py
+++ wc_rules/canonical.py
@@ -1,43 +1,86 @@
 from functools import reduce
+from collections import defaultdict,deque
 from pprint import pformat
+from copy import deepcopy
 
 def canonize(g):
-	# g is a scaffold
-	equivalence_classes = compute_equivalence_classes(g)
-	print(pformat(equivalence_classes))
+
+	partition = compute_initial_partition(g)
+	if len(partition)!=len(g):
+		new_partition = deque()
+		old_length = len(partition)
+		while partition:
+			cell = partition.popleft()
+			celldeque = partition_current_cell(new_partition,cell,g)
+			print(new_partition,celldeque,partition)
+			if len(celldeque)==1:
+				new_partition += celldeque
+			else:
+				partition += celldeque
+			if len(partition)==0 and len(new_partition) > old_length:
+				partition = new_partition
+				old_length=len(partition)
+				new_partition = deque()
+			print(partition)		
+		partition = new_partition
+	print(partition)
 	return True
 
-def sequentiate_dicts(*dicts,keys=None):
-	# for dicts d1,d2,d3... returns a dict such that
-	# out[key] = d3[d2[d1[key]]]
-	if keys is None:
-		keys= dicts[0].keys()
-	return {key:reduce(lambda key,d: d[key],dicts,key) for key in keys}
+def partition_current_cell(partition,cell,g):
+	# if cell is compatible and doesn't need to be subdivided,
+	# return deque([cell])
+	# else: partition into smaller cells and return deque([cell1,cell2,cell3])
+	if len(cell)==1:
+		return deque([cell])
 
-def renumber_by_value(d):
-	# indexes unique elements from d.values,
-	# then, replaces values in d by index
-	# e.g. dict(x='a',y='a',z='b') --> dict(x=0,y=0,z=1)
-	v = {x:i for i,x in enumerate(sorted(set(d.values())))}
-	return sequentiate_dicts(d,v)
-
-def compute_equivalence_classes(g):
-	# this is the initial step of sorting nodes
-	# in some initial set of equivalence classes
-	eqv = dict()
-	for node in g:
-		eqv[node.id] = compute_signature(node)
-	return renumber_by_value(eqv)
-
-def compute_signature(node):
-	# signature determines the initial sorting of nodes
-	# the current signature is degree,classname,sorted_individual_edges
-	_cls = node.__class__.__name__
-	edges = []
-	for attr in node.get_related_attributes():
-		edges.extend([(attr,x.__class__.__name__) for x in node.listget(attr)])
-
-	edges = tuple(sorted(edges))
-	signature = (len(edges),_cls,edges)
-	return signature
+	indexes = index_partition(partition+deque([cell]))
+	certs = defaultdict(list)
+	for node in cell:
+		sgn = get_neighbor_signature(node,indexes,g)
+		certs[ sgn ].append(node)
 	
+	cells = [certs[sgn] for sgn in sorted(certs)]
+	return deque(cells)
+
+def get_neighbor_signature(node,indexes,g):
+	neighbors = []
+	for attr in g[node].get_nonempty_related_attributes():
+		for neighbor in g[node].listget(attr):
+			if neighbor.id in indexes:
+				neighbors.append((attr,indexes[neighbor.id]))
+	return tuple(sorted(neighbors))
+
+def index_partition(partition):
+	indexes = dict()
+	for i,cell in enumerate(partition):
+		for node in cell:
+			indexes[node] = i
+	return indexes	
+
+
+
+def compute_initial_partition(g):
+	# create a signature for each node
+	# (degree,class_name,sorted_edge_tuples)
+	# .... sorted_edge_tuples = (attr,destination_class), (attr,destination_class), ...
+	# collect nodes with same signature
+	eqv = defaultdict(list)
+	for node in g:
+		edges = [(attr,x.__class__.__name__) for attr in node.get_nonempty_related_attributes() for x in node.listget(attr)]
+		edges = tuple(sorted(edges))
+		sgn = (len(edges),node.__class__.__name__,edges) 
+		eqv[ sgn ].append(node.id)
+	return deque([ sorted(eqv[sgn]) for sgn in sorted(eqv) ])
+
+
+
+def invert_map(d):
+	# {v:[k1,k2..] for k:v}
+	out = dict({v:[] for v in set(d.values())})
+	for k,v in d.items():
+		out[v].append(k)
+	return dict(out)
+
+def invert_bmap(d):
+	# {v:k for k:v} use carefully
+	return {v:k for k,v in b.items()}
