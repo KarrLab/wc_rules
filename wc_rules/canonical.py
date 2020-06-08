8a79b1f15cc5e447d02b6f9d430f900703e47597
John Sekar <johnarul.sekar@gmail.com>
2020-06-08 08:36:38 -0400

    documentation for canonical ordering

--- wc_rules/canonical.py
+++ wc_rules/canonical.py
@@ -4,23 +4,78 @@ from collections import defaultdict,deque
 from pprint import pformat
 import math
 
+##### DESCRIPTION ####################
+# An ordered partition is a list of cells of nodes of a graph: 
+# {de}{abc}{f} is an OP of a graph {abcdef} with edges ad, ae, bd, be, cd, ce, df, ef.
+# An OP is "equitable" if each cell's nodes have identical relationships to other cells:
+# 	nodes in {de} each have 3 edges to {abc} and 1 to {f}
+# 	nodes in {abc} each have 2 edges to {de} 
+# 	node in {f} has 2 edges to {de}.
+# The coarsest equitable partition CEP == each cell is a node orbit {de}{abc}{f}.
+# The finest equitable partition FEP == an ordering of nodes {d}{e}{a}{b}{c}{f}.
+# To find CEP, refine an initial deterministically ordered partition until its equitable.
+# To find FEP, sequentially break-and-refine cells in a CEP until all cells are singletons.
+
+##### DETAILS ##########################
+# index_partition() 
+#	input: a partition
+#	output: each node receives the index of its cell in the partition
+# node_certificate()
+#	input: a node, a partition's index, graph
+#	output: a certificate characterizing the node's relationship to nodes in other cells
+# CertificateCounter(), groups()
+# 	input: a list of nodes, a certificate function
+#	output: partitions nodes into groups with matching certificates
+# partition_cell()
+# 	input: a cell in a partition, graph
+#	output: breaks the cell into smaller cells if cell nodes have non-matching certificates
+# refine_partition()
+#	input: initial partition
+#	output: sequentially calls partition_cell and reindexing until all cells have matching certificates
+# break_and_refine()
+# 	input: an partition with at least one non-singleton cell 
+# 	output: separates lexicographic leader {abc}->{a},{bc}, refines until equitable,
+#	(tracks breaking procedure using a leaders dict a:{bc})
+# canonical_ordering()
+#	input: graph
+#	ouput: CEP (ordered by FEP), leaders
+
+
 class CertificateCounter(defaultdict):
 	def __init__(self,fn,_list):
-		# applies fn to each element of _list to create a certificate
-		# then collects groups with the same certificate
-		# maintains sorting order of _list
 		super().__init__(list)
 		for elem in _list:
 			self[fn(elem)].append(elem)
 
 	def groups(self):
-		# returns groups in sorted order of certificates
-		#print([[key,self[key]] for key in sorted(self)])
 		return [self[key] for key in sorted(self)]
 
+####### methods for generating a canonical partition/ordering of a graph container
+def canonical_ordering(g):
+	# partition = coarsest equitable partition
+	partition = refine_partition(g,initial_partition(g))
+	p, leaders = partition.copy(), dict()	
+	while len(p) < len(g):
+		p, leader, remaining = break_and_refine(g,p)
+		leaders[leader] = remaining
 
+	order = [x for y in p for x in y]
+	#order = canonical ordering
+	for x in partition:
+		x.sort(key = lambda x: order.index(x))
 
-####### methods for generating a canonical partition of a graph container
+	return partition,leaders
+
+def break_and_refine(g,p):
+	# identify first non-singleton cell idx, 
+	# separate its lexicographic leader and call refine_partition
+	idx = [i for i,x in enumerate(p) if len(x)>1][0]
+	leader, remaining = p[idx][0], p[idx][1:]
+	p[idx:idx+1] = [ [leader], remaining ]
+	p = refine_partition(g,p)
+	return p, leader, remaining  
+		
+
 def refine_partition(g,p):
 	# g is a graph, p is an ordered partition
 	rhs,lhs,modified = deque(p), deque(), False
@@ -34,35 +89,6 @@ def refine_partition(g,p):
 		if len(rhs)==0 and modified:
 			rhs, lhs, modified = lhs, deque(), False
 	return list(lhs)
-
-
-def canonical_ordering(g):
-	# initially partition based on node classes and degrees
-	# then refine until valid
-	partition = refine_partition(g,initial_partition(g))
-	# now cells == orbits, but individual orbits need to be ordered
-
-	p, leaders = partition.copy(), dict()
-	# until all orbits broken
-	while len(p) < len(g):
-		# find first non-trivial orbit
-		# separate its lexicographic leader
-		# refine until valid
-		idx = [i for i,x in enumerate(p) if len(x)>1][0]
-		leader, remaining = p[idx][0], p[idx][1:]
-		leaders[leader] = remaining
-		p[idx:idx+1] = [ [leader], remaining ]  
-		p = refine_partition(g,p)
-
-	order = [x for y in p for x in y]
-
-	# use acquired order to sort partition cells
-	for x in partition:
-		x.sort(key = lambda x: order.index(x))
-
-	return partition,leaders
-
-
 
 def index_partition(partition):
 	# maps each idx in g -> index of cell containing idx in partition
@@ -96,9 +122,3 @@ def initial_node_certificate(idx,g):
 	attrs = node.get_nonempty_related_attributes()
 	edges = [(a,x.__class__.__name__) for a in attrs for x in node.listget(a)]	
 	return (len(edges),node.__class__.__name__,tuple(sorted(edges)))
-
-
-def invert_map(d):
-	# assume map is one to one
-	return {y:x for x,y in d.items()}
-
