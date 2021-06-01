34e1ab5fc6cd6f757eef2ee5834fd4d570d1ca21
John Sekar <johnarul.sekar@gmail.com>
2021-06-01 14:03:30 -0400

    Dedicated Edge object in graph.collections

--- wc_rules/graph/collections.py
+++ wc_rules/graph/collections.py
@@ -1,6 +1,24 @@
 from dataclasses import dataclass
 from ..utils.collections import DictLike
+from typing import Tuple
 
+@dataclass(order=True,frozen=True)
+class Port:
+    __slots__ = ['node','attr']
+    node: str
+    attr: str
+
+@dataclass(order=True,frozen=True)
+class Edge:
+    __slots__ = ['ports']
+    ports: Tuple[Port]
+
+    @staticmethod
+    def make(n1,e1,n2,e2):
+        return Edge(tuple(sorted([Port(n1,e1),Port(n2,e2)])))
+
+    def unpack(self):
+        return (self.ports[0].node, self.ports[0].attr, self.ports[1].node, self.ports[1].attr,)
 
 class GraphContainer(DictLike):
     # A temporary container for a graph that can be used to create
@@ -20,8 +38,7 @@ class GraphContainer(DictLike):
             for attr in node.get_related_attributes(ignore_None=True):
                 related_attr = node.get_related_name(attr)
                 for related_node in node.listget(attr):
-                    edge = [(node.id, attr), (related_node.id,related_attr)]
-                    edge = tuple(sorted(edge))
+                    edge = Edge.make(node.id, attr,related_node.id,related_attr)
                     if edge not in edges_visited:
                         edges_visited.add(edge)
                         yield edge
@@ -42,7 +59,8 @@ class GraphContainer(DictLike):
             idx = varmap.get(idx,idx)
             newnodes[idx] = node.duplicate(id=idx)
 
-        for (idx1,attr1),(idx2,attr2) in self.iteredges():
+        for edge in self.iteredges():
+            idx1,attr1,idx2,attr2 = edge.unpack()
             if idx1 not in include or idx2 not in include:
                 continue
             idx1, idx2 = [varmap.get(x,x) for x in [idx1,idx2]]
@@ -72,6 +90,7 @@ class GraphContainer(DictLike):
     def add_suffix(self,suffix):
         varmap = {x:f'{x}_{suffix}' for x in self.keys()}
         return self.duplicate(varmap=varmap)
+
 
 @dataclass(unsafe_hash=True)
 class CanonicalForm:
