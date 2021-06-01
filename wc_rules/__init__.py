0c7e29e687303683bf0715eb53a56f20a12f9788
John Sekar <johnarul.sekar@gmail.com>
2021-06-01 14:03:12 -0400

    previous

--- wc_rules/__init__.py
+++ wc_rules/__init__.py
@@ -1,20 +1,20 @@
-"""
-:Author: John Sekar <johnarul.sekar@gmail.com>
-:Date: 2017-12-13
-:Copyright: 2017, Karr Lab
-:License: MIT
-"""
+# """
+# :Author: John Sekar <johnarul.sekar@gmail.com>
+# :Date: 2017-12-13
+# :Copyright: 2017, Karr Lab
+# :License: MIT
+# """
 
-import pkg_resources
+# import pkg_resources
 
-with open(pkg_resources.resource_filename('wc_rules', 'VERSION'), 'r') as file:
-	__version__ = file.read().strip()
-# :obj:`str`: version
+# with open(pkg_resources.resource_filename('wc_rules', 'VERSION'), 'r') as file:
+# 	__version__ = file.read().strip()
+# # :obj:`str`: version
 
-# API
-# from . import base
-# from . import entity
-# from . import chem
-# from . import graph_utils
-# from . import seq
-# from . import bioseq
+# # API
+# # from . import base
+# # from . import entity
+# # from . import chem
+# # from . import graph_utils
+# # from . import seq
+# # from . import bioseq
