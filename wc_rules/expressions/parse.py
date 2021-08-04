f17fa4a0cff0ba406a06e857679289a7409dd155
John Sekar <johnarul.sekar@gmail.com>
2021-08-03 23:19:09 -0400

    Moving expression grammar to a separate file

--- wc_rules/expressions/parse.py
+++ wc_rules/expressions/parse.py
@@ -2,60 +2,10 @@ from lark import Lark, tree, Transformer,Visitor, v_args, Tree,Token
 from ..utils.collections import merge_lists, merge_dicts, pipe_map,listmap
 from operator import itemgetter,attrgetter
 from functools import partial
+from pathlib import Path
 
-
-grammar = """
-%import common.CNAME
-%import common.WS_INLINE
-%import common.NUMBER
-%ignore WS_INLINE
-%import common.ESCAPED_STRING
-%import common.NEWLINE
-
-COMMENT: /#.*/
-%ignore COMMENT
-%ignore NEWLINE
-
-    ?literal.1: NUMBER -> number | "True"-> true | "False" -> false | ESCAPED_STRING -> string
-    ?atom: literal | function_call |  "(" expression ")" 
-    
-    variable: CNAME
-    attribute: CNAME
-
-    arg: expression
-    kw: CNAME
-    kwarg: kw "=" arg
-    args: arg ("," arg )*
-    kwargs: kwarg ("," kwarg)*
-    
-    function_name: CNAME
-    subvariable: CNAME
-    function_call: variable ["." subvariable] "." function_name  "(" [args|kwargs] ")"
-        | variable ["." subvariable] "." attribute
-        | function_name "(" [args|kwargs] ")"
-        | variable 
-        
-    ?sum: term (add_op term)*
-    ?term: factor (mul_op factor)* 
-    ?factor: factor_op factor | atom
-
-    ?factor_op: "+" -> noflip | "-" -> flipsign
-    ?add_op: "+" -> add | "-" -> subtract
-    ?mul_op: "*" -> multiply | "/" -> divide    
-
-    ?expression: sum
-
-    boolean_expression: expression bool_op expression
-    
-    ?bool_op: ">=" -> geq | "<=" -> leq | ">" -> ge | "<" -> le | "==" -> eq | "!=" -> ne
-    
-    assignment: declared_variable "=" (expression|boolean_expression)
-    declared_variable: CNAME
-
-    ?start: (assignment|boolean_expression)
-"""    
-    #expressions: (assignment|boolean_expression) (NEWLINE (assignment|boolean_expression))* 
-    #?start: [NEWLINE] expressions [NEWLINE]
+grammarfile = Path(__file__).parent / 'expression.lark'
+grammar = grammarfile.read_text()
 
 ### process constraint string
 def process_expression_string(string_input,start='start'):
